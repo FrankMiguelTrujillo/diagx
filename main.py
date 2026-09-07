2266af930954ccb9ad7d8592ce5f7c3764bb4231
Frank Trujillo <franktrujillo92929@gmail.com>
2026-09-06 19:03:01 -0500

    feat: add SQLAlchemy in api environment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 from metodos_solid import create_detectors
@@ -8,6 +8,16 @@ from metodos_solid import LowTrafficDetail
 from metodos_solid import ManagementDetail
 from metodos_solid import BadReputationDetail
 from datetime import datetime
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import models
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 businesses_db = {}
@@ -31,10 +41,16 @@ def root():
     return {"status": "Diagx API running"}
 
 @app.post("/businesses")
-def create_business(business: BusinessBasic):
-    businesses_db[business.id] = business
-    return business
-
+def create_business(business: BusinessBasic, db: Session = Depends(get_db)):
+    db_business = models.Business(
+        name=business.name,
+        sector=business.sector,
+        monthly_revenue=business.monthly_revenue
+    )
+    db.add(db_business)
+    db.commit()
+    db.refresh(db_business)
+    return db_business
 @app.get("/businesses/{id}")
 def get_business(id: UUID):
     if id not in businesses_db:
@@ -67,5 +83,4 @@ def get_diagnostics_history(id: UUID):
         raise HTTPException(status_code=404, detail="Business not found")
     
       return diagnostics_db.get(id, []) 
-  
   
