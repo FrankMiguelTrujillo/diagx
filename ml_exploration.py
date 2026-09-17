da5f84a67070925a34c5b7231ee678bc9aaf9083
Frank Trujillo <franktrujillo92929@gmail.com>
2026-09-16 21:34:45 -0500

    feat: cleaning datas

--- ml_exploration.py
+++ ml_exploration.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("data/nonbankrupt_retail_16_year_13_14_15.csv", sep=";") 
-print(df.shape)        # ¿cuántas filas y columnas tiene?
-print(df.head())       # las primeras filas, para ver cómo se ven los datos
-print(df.info())       # tipos de datos, valores nulos por columna
+df = pd.read_csv("data/nonbankrupt_retail_16_year_13_14_15.csv", sep=";", decimal=",") 
+print(f"cuantas filas y columnas tiene?: {df.shape}")        # ¿cuántas filas y columnas tiene?
+print(f"primeras filas: {df.head()}")       # las primeras filas, para ver cómo se ven los datos
+print(f"tipos de datos: {df.info()}")  
+print(df.isnull().sum().sort_values(ascending=False))# tipos de datos, valores nulos por columna
+umbral = 0.3 * len(df)
+df_limpio = df.dropna(axis=1, thresh=len(df) - umbral)
+print(df_limpio)
